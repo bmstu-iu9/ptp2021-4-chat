aced7aba066bc2edd5adcdc440b341329d07c53c
postlog <scarfaceborn@mail.ru>
2021-07-15 21:44:39 +0300

    Изменение ширины отступа с 4 пробелов до 2 и удаление лишней функции

--- gulpfile.js
+++ gulpfile.js
@@ -8,63 +8,58 @@ const clean = require('gulp-clean')
 
 
 const paths = {
-    sources: {
-        allFiles: 'src/**/*.*',
-        root: 'src/',
-        assets: 'src/assets/**/*.*',
-        html: 'src/*.html',
-        styles: 'src/styles/**/*.css',
-        js: 'src/js/**/*.js'
-    },
-    destinations: {
-        allFiles: 'public/**/*.*',
-        root: 'public/',
-        assets: 'public/assets/',
-        html: 'public/',
-        styles: 'public/styles/',
-        js: 'public/js'
-    }
-}
-
-
-function exclude(path) {
-    return '!' + path
+  sources: {
+    allFiles: 'src/**/*.*',
+    root: 'src/',
+    assets: 'src/assets/**/*.*',
+    html: 'src/*.html',
+    styles: 'src/styles/**/*.css',
+    js: 'src/js/**/*.js'
+  },
+  destinations: {
+    allFiles: 'public/**/*.*',
+    root: 'public/',
+    assets: 'public/assets/',
+    html: 'public/',
+    styles: 'public/styles/',
+    js: 'public/js'
+  }
 }
 
 function buildStyles() {
-    return gulp.src(paths.sources.styles)
-        .pipe(autoprefixer())
-        .pipe(minify())
-        .pipe(concat('bundle.min.css'))
-        .pipe(gulp.dest(paths.destinations.styles))
+  return gulp.src(paths.sources.styles)
+    .pipe(autoprefixer())
+    .pipe(minify())
+    .pipe(concat('bundle.min.css'))
+    .pipe(gulp.dest(paths.destinations.styles))
 }
 
 function buildJs() {
-    return gulp.src(paths.sources.js)
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.destinations.js))
+  return gulp.src(paths.sources.js)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.destinations.js))
 }
 
 function buildHtml() {
-    return gulp.src(paths.sources.html)
-        .pipe(gulp.dest(paths.destinations.html))
+  return gulp.src(paths.sources.html)
+    .pipe(gulp.dest(paths.destinations.html))
 }
 
 function buildAssets() {
-    return gulp.src(paths.sources.assets)
-        .pipe(gulp.dest(paths.destinations.assets))
+  return gulp.src(paths.sources.assets)
+    .pipe(gulp.dest(paths.destinations.assets))
 }
 
 function cleanup() {
-    return gulp.src(paths.destinations.allFiles)
-        .pipe(clean())
+  return gulp.src(paths.destinations.allFiles)
+    .pipe(clean())
 }
 
 gulp.task('watch', () => {
-    gulp.watch(paths.sources.allFiles, gulp.series('build'))
+  gulp.watch(paths.sources.allFiles, gulp.series('build'))
 })
 
 
