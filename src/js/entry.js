ce7c515d87ead0e3f9ba0d98e056d562ada934d0
Mikhail Kozochkin <51835188+Postlog@users.noreply.github.com>
2021-07-25 18:39:41 +0300

    Избавление от ненужного аргумента username в функции validatePassword

--- src/js/entry.js
+++ src/js/entry.js
@@ -16,6 +16,6 @@ registrationFormSwitchButton.addEventListener('click', function(event) {
   loginForm.classList.remove('form_hidden')
 })
 
-function validatePassword(username, password) {
+function validatePassword(password) {
   return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/.test(password)
 }
