f84f057b7eb6143226798762a575175786b8ed70
Mikhail Kozochkin <51835188+Postlog@users.noreply.github.com>
2021-07-29 13:22:27 +0300

    Добавление полноценной валидации имени и пароля

--- server/middleware/usernameAndPassword.js
+++ server/middleware/usernameAndPassword.js
@@ -1,15 +1,19 @@
-const {passwordMinLength} = require('../constants')
-const {errors} = require('../../common/common')
 const {createError} = require('../misc/utils')
+const {
+  errors,
+  validateUsername,
+  validatePassword
+} = require('../../common/common')
+
 
 
 function sendErrorIfNotProvidedOrNotValid(request, response, next) {
   const {username, password} = request.body
 
-  if (!checkProvided(username, password) || password.length < passwordMinLength) {
-    const error = createError(...Object.values(errors.incorrectBody))
-    return response.status(error.code)
-    .send(error.message)
+  if (!checkProvided(username, password)
+    || !validateUsername(username)
+    || !validatePassword(password)) {
+    throw createError(...Object.values(errors.incorrectBody))
   }
 
   next()
@@ -19,9 +23,7 @@ function sendErrorIfNotProvided(request, response, next) {
   const {username, password} = request.body
 
   if (!checkProvided(username, password)) {
-    const error = createError(...Object.values(errors.incorrectBody))
-    return response.status(error.code)
-    .send(error.message)
+    throw createError(...Object.values(errors.incorrectBody))
   }
 
   next()
@@ -33,6 +35,7 @@ function checkProvided(login, password) {
   }
   return !(typeof login !== 'string' || typeof password !== 'string')
 }
+
 
 module.exports = {
   sendErrorIfNotProvidedOrNotValid,
