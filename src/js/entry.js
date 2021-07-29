ec073c72b72f51d8744db97ded63ef639dfd0997
pollykon <pollykon@yandex.ru>
2021-07-29 21:11:50 +0300

    Добавление перечня ошибок

--- src/js/entry.js
+++ src/js/entry.js
@@ -2,10 +2,65 @@ const authForm = document.querySelector('.auth-form')
 const registrationForm = document.querySelector('.registration-form')
 const authFormSwitchButton = document.querySelector('.auth-form__switch-button')
 const registrationFormSwitchButton = document.querySelector('.registration-form__switch-button')
+const authFormSubmitButton = document.querySelector('.auth-form__submit-button')
+const registrationFormSubmitButton = document.querySelector('.registration-form__submit-button')
+
 
 const notificationWindow = document.querySelector('.notification-window')
 const notificationWindowTitle = document.querySelector('.notification-window__title')
 const notificationWindowParagraph = document.querySelector('.notification-window__paragraph')
+
+const errorsMessages = {
+  fieldsEmpty: {
+    title: 'Ошибка',
+    message: () => 'Поля не могут быть пустыми',
+    asError: true
+  },
+  passwordsDontMatch: {
+    title: 'Ошибка',
+    message: () => 'Пароли не совпадают',
+    asError: true
+  },
+  invalidUsername: {
+    title: 'Ошибка',
+    message: () => 'Имя пользователя должно иметь длину от 3 до 27 символов и содержать ' +
+      'только буквы латинского алфавита и цифры',
+    asError: true
+  },
+  invalidPassword: {
+    title: 'Ошибка',
+    message: () => 'Пароль должен иметь длину от 6 символов и содержать ' +
+      'минимум 1 строчную, 1 заглавную буквы латинского алфавита и 1 цифру',
+    asError: true
+  },
+  connectionError: {
+    title: 'Ошибка',
+    message: () => 'Не удалось подключиться к серверу',
+    asError: true
+  },
+  incorrectPassword: {
+    title: 'Ошибка',
+    message: () => 'Неверный пароль',
+    asError: true
+  },
+  usernameNotRegistered: {
+    title: 'Уведомление',
+    message: (username) => `Такой пользователь не зарегистрирован, вы можете` +
+      ` перейти на <a href="/entry?action=registration&username=${username}">страницу регистрации</a>`,
+    asError: false
+  },
+  usernameAlreadyRegistered: {
+    title: 'Уведомление',
+    message: (username) => `Вы уже ранее регистрировались,` +
+      ` перейти на <a href="/entry?action=auth&username=${username}">страницу авторизации</a>?`,
+    asError: false
+  },
+  defaultError: {
+    title: 'Ошибка',
+    message: (message) => `Неизвестная ошибка: ${message}`,
+    asError: true
+  }
+}
 
 authFormSwitchButton.addEventListener('click', function(event) {
   event.preventDefault()
