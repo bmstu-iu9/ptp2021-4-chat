acdc2313641a38c0b4997f8c126582cdef27e802
pug-coder <andrey.suchanov@yandex.ru>
2021-08-13 16:14:25 +0300

    Добавление модели Content

---
+++ server/models/content.js
@@ -0,0 +1,28 @@
+const {Model, DataTypes} = require('sequelize')
+const {sequelize} = require('../definitions')
+
+
+class Content extends Model {
+}
+
+
+Content.init({
+  type: {
+    type: DataTypes.ENUM('text', 'voice'),
+    allowNull: false
+  },
+  value: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  files: DataTypes.ARRAY(DataTypes.TEXT)
+}, {
+  sequelize,
+  modelName: 'content',
+  timestamps: false
+})
+
+
+module.export = {
+  Content
+}
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 require('./user')
 require('./session')
 require('./conversation')
+require('./content')
