3d41cf6727fc72bec68d2ea19d93401d15b2c56c
EliseySoft <el2002@inbox.ru>
2021-08-17 21:08:30 +0500

    Добавление функции получения пользователя

---
+++ server/ws/services/user.js
@@ -0,0 +1,24 @@
+const {User} = require("../../database/models/user");
+const {Session} = require("../../database/models/session");
+
+
+async function getUser(sessionId) {
+    const session = await Session.findOne({
+        where: {
+            sessionId
+        }
+    })
+
+    const user = await User.findOne({
+        where: {
+            id : session.userId
+        }
+    })
+
+    return user
+}
+
+
+module.exports = {
+    getUser
+}
