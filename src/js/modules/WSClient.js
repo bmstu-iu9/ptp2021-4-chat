f12f1b0ae8e0b630b414137d6f7caad8f8864103
boomb0om <igorekandco1@gmail.com>
2021-08-22 18:04:37 +0300

    Обновлен класс-контейнер WSClient

--- src/js/modules/WSClient.js
+++ src/js/modules/WSClient.js
@@ -1,6 +1,4 @@
-
 function randomInteger(min, max) {
-  // случайное число от min до (max+1)
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
@@ -9,21 +7,17 @@ function generateID() {
   return randomInteger(1, 2**64)
 }
 
-class WSClient {
+
+export default class WSClient {
   #socket
   #pendingRequests
+  #messageHandler
+  #closeHandler
+  url
 
   constructor(url) {
-    this.#socket = new WebSocket(url)
+    this.url = url
     this.#pendingRequests = {}
-
-    const responseHandler = this.#handleResponse
-    this.#socket.onmessage = function(event) {
-      responseHandler(event)
-    }
-    this.#socket.onclose = function(event) {
-      // обработка разрыва соединения
-    }
   }
 
   #generateUniqueID() {
@@ -31,53 +25,110 @@ class WSClient {
     do {
       id = generateID()
     } while (!(id in this.#pendingRequests))
+
     return id
   }
 
-  #handleResponse(event) {
+  connect() {
+    this.#socket = new WebSocket(this.url)
+
+    this.#socket.onmessage = (event) => {
+      this.#handleOnMessage(event)
+    }
+
+    this.#socket.onclose = (event) => {
+      if (this.#closeHandler) {
+        this.#closeHandler(event.code, event.reason)
+      }
+    }
+
+    let resolved = false
+    return new Promise((resolve, reject) => {
+      this.#socket.onopen = () => {
+        resolve()
+        resolved = true
+      }
+
+      this.#socket.onerror = () => {
+        if (!resolved) {
+          return reject()
+        }
+
+        this.#handleOnError()
+      }
+    })
+  }
+
+  #handleOnError() {
+    for (const controls of Object.values(this.#pendingRequests)) {
+      controls.reject({
+        code: 228,
+        message: "Ошибка при подключении"
+      })
+    }
+
+    this.#pendingRequests = {}
+  }
+
+  #handleOnMessage(event) {
     let message
     try {
       message = JSON.parse(event.data)
     } catch {
       throw new Error("Сервер ответил не в JSON формате")
     }
-    const id = message.id
-    const data = message.data
-    if (!(id && data)) {
+
+    const id = message.$id
+    const payload = message.payload
+
+    if (!Number.isInteger(id) && this.#messageHandler) {
+      return this.#messageHandler(message)
+    }
+
+    if (!payload) {
       throw new Error("JSON, который вернул сервер, имеет неправильный формат.")
     }
     if (!(id in this.#pendingRequests)) {
-      return
+      throw new Error("Сервер вернул сообщение с несуществующим id")
     }
-    //Обработка event
-    if (data.error) {
-      this.#pendingRequests[id].reject(new Error(JSON.stringify(data.error)))
+
+    // Обработка event
+    if (payload.error) {
+      this.#pendingRequests[id].reject(payload.error)
     } else {
-      this.#pendingRequests[id].resolve(data)
+      this.#pendingRequests[id].resolve(payload)
     }
     delete this.#pendingRequests[id]
   }
 
   send(data) {
+    if (this.#socket.readyState !== 1) {
+      return new Promise.reject({
+        code: this.#socket.readyState,
+        message: "Нет соединения с сервером"
+      })
+    }
+
     const id = this.#generateUniqueID()
-    let requestToAdd = this.#pendingRequests[id] = {}
-    const promise = new Promise(function(resolve, reject) {
-      requestToAdd[id].resolve = resolve
-      requestToAdd[id].reject = reject
+    const promise = new Promise((resolve, reject) => {
+      this.#pendingRequests[id].resolve = resolve
+      this.#pendingRequests[id].reject = reject
     })
+
     const message = {
       "$id": id,
       "payload": data
     }
     this.#socket.send(JSON.stringify(message))
+
     return promise
   }
 
   setOnMessageHandler(handler) {
-    const responseHandler = this.#handleResponse
-    this.#socket.onmessage = function(event) {
-      responseHandler(event)
-      handler(event)
-    }
+    this.#messageHandler = handler
+  }
+
+  setOnCloseHandler(handler) {
+    this.#closeHandler = handler
   }
 }
