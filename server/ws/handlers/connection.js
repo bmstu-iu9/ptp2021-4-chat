e39f77956705f2db62539db6b858e25143eb5e9d
Mikhail Kozochkin <51835188+Postlog@users.noreply.github.com>
2021-08-26 16:09:59 +0300

    Отправка редиректа в случае некорректной сессии в WS-обработчике события connection

--- server/ws/handlers/connection.js
+++ server/ws/handlers/connection.js
@@ -1,14 +1,21 @@
+const {wssClientsContexts} = require('../../definitions')
+const {WSError} = require('../../misc/wsErrors')
 const {wss, wssClients} = require('../../definitions')
 
 
 wss.onConnection(context => {
   const {current, socket} = context
-
+  const redirectUrl = context.redirectUrl
   const sessionId = current.session.sessionId
 
+  if (redirectUrl) {
+    throw new WSError(4000, {redirectUrl})
+  }
+
   if (sessionId in wssClients) {
-    wssClients[sessionId].close(1007, 'Было открыто новое соединение')
+    wssClients[sessionId].modifiedClose(1007, {message: 'Было открыто новое соединение'})
   }
 
   wssClients[sessionId] = socket
+  wssClientsContexts[sessionId] = context
 })
