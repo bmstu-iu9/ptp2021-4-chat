d216235bd9841a9ce660bcb2cd4959978c5cd231
Mikhail Kozochkin <51835188+Postlog@users.noreply.github.com>
2021-08-26 16:09:04 +0300

    Удаление временных сессий в WS-обработчике события close

--- server/ws/handlers/close.js
+++ server/ws/handlers/close.js
@@ -1,7 +1,21 @@
+const {wssClientsContexts} = require('../../definitions')
+const {deleteSession} = require('../../http/services/logout')
 const {wssClients} = require('../../definitions')
 const {wss} = require('../../definitions')
 
 
-wss.onClose((context) => {
-  delete wssClients[context.current.session.sessionId]
+wss.onClose(async context => {
+  if (context.socket.isClosedByServer()) {
+    return
+  }
+
+  const session = context.current.session
+  const sessionId = session.sessionId
+
+  if (!session.expirationDate) {
+    await deleteSession(sessionId)
+  }
+
+  delete wssClients[sessionId]
+  delete wssClientsContexts[sessionId]
 })
