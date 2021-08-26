4bb8ad95cc5ae305f09fab55ec3297e0dcf34a26
Mikhail Kozochkin <51835188+Postlog@users.noreply.github.com>
2021-08-26 16:08:32 +0300

    Изменение обработчиков ошибок с использованием modifiedClose

--- server/ws/handlers/error.js
+++ server/ws/handlers/error.js
@@ -1,30 +1,30 @@
-const WSError = require("../../misc/WSError")
+const {WSError} = require('../../misc/wsErrors')
 const {isDev} = require('../../config')
-const {logger} = require("../../definitions")
-const {wss} = require("../../definitions");
+const {logger} = require('../../definitions')
+const {wss} = require('../../definitions')
 
 
 wss.onError((context, error, next) => {
-    const socket = context.socket
+  const socket = context.socket
 
-    if (error instanceof WSError && socket.readyState === socket.OPEN) {
-        if (isDev) {
-            console.log('Ошибка запроса', error.stack)
-        }
-
-        socket.close(1007, error.message)
-        return
+  if (error instanceof WSError && socket.readyState === socket.OPEN) {
+    if (isDev) {
+      console.log('Ошибка запроса', error.stack)
     }
 
-    next()
+    socket.modifiedClose(error.code, error.payload)
+    return
+  }
+
+  next()
 })
 
 wss.onError((context, error) => {
-    const socket = context.socket
+  const socket = context.socket
 
-    if (socket.readyState === socket.OPEN) {
-        socket.close(1011)
-    }
+  if (socket.readyState === socket.OPEN) {
+    socket.modifiedClose(1011, {message: 'Неизвестная ошибка'})
+  }
 
-    logger.error(error.stack)
+  logger.error(error.stack)
 })
